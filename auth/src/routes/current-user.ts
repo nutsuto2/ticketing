f275e6950abf84a260968a371435e0e4dc023c6f
summer <nut22_ks@hotmail.com>
2024-04-01 16:39:59 +0700

    add require-auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import { requireAuth } from '../middlewares/require-auth';
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req, res) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
     res.send({ currentUser : req.currentUser || null });
 });
 
