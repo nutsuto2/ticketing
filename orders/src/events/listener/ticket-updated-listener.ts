3701196bf998e848862894dd65682e700d501d3c
summer <nut22_ks@hotmail.com>
2024-05-20 22:26:39 +0700

    update record versioning to orders service

--- orders/src/events/listener/ticket-updated-listener.ts
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -8,7 +8,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-        const ticket = await Ticket.findById(data.id);
+        const ticket = await Ticket.findByEvent(data);
 
         if (!ticket) {
             throw new Error('Ticket not found');
