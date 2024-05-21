03c9a866916e1f52eb794532e82c497cc243cb03
summer <nut22_ks@hotmail.com>
2024-05-22 00:37:23 +0700

    update base listener and publisher class also implement edit reserved function

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided id does not exist', async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -126,3 +127,28 @@ it('publishes as event', async () => {
     
     expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
+
+it('rejects updates if ticket is reserved', async () => {
+    const cookie = global.signin()
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'book',
+            price: 20
+        });
+    
+    const ticket = await Ticket.findById(response.body.id);
+    ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+    await ticket!.save();
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new book',
+            price: 200
+        })
+        .expect(400);
+})
